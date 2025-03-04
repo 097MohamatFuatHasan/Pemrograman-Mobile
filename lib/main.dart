58f748708d5dc2d8143b3e2e3ca012daef4c47e5
23091397081_Jonathan Chandra Ivanta <149121637+JonathanChandra@users.noreply.github.com>
2025-03-05 01:26:52 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,4 @@
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("I am half-human, half-Khong Guan"),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/Mascot.png', width: 150),
-              const SizedBox(height: 20),
-              Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.blue[100],
-                  borderRadius: BorderRadius.circular(10),
-                ),
                 child: const Column(
                   children: [
                     Text(" 🍪 Tetap Percaya Kaleng Khong Guan itu berisi roti",
